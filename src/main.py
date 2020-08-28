f6eaf1b107551e69fbfb6ec01234b31706c09e14
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-08-28 11:56:23 +0200

    [ADD] - drawComponents function drawing alive cells and scaling them

--- src/main.py
+++ src/main.py
@@ -1,26 +1,26 @@
 import sys, pygame
 
-blackCell = pygame.image.load("../img/black.png")
-
 def main():
     pygame.init()
-    size = width, height = 1024, 768
+    size = width, height = (1024, 768)
     screen = pygame.display.set_mode(size)
 
     gameLoop(screen)
 
 def gameLoop(screen):
-    white = 255, 255, 255
-    
+    white = (255, 255, 255)
+    blackCell = pygame.image.load("../img/black.png")
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         screen.fill(white)
-        #drawComponents(screen)
+        drawComponents(screen, blackCell)
         pygame.display.flip()
 
-def drawComponents(screen):
-    print("drawComponents function")
+def drawComponents(screen, blackCell):
+    blackCell = pygame.transform.scale(blackCell, (10,10))
+    screen.blit(blackCell, (10, 10))
 
 if __name__ == "__main__":
     main()
