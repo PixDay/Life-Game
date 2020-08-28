f5a7a2d60328c2852729c70c28c6fc1004b1a230
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-08-28 13:40:17 +0200

    [ADD] - First working game life on a fixed size and fixed data

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 import sys, pygame
+import math 
+
+#imageData = pygame.image.load("../map/3x3.png") # loading image
+#datas = pygame.image.tostring(imageData, "RGBA")
+#print(datas)
+#print(len(datas) / 4)
+
 
 def main():
     pygame.init()
@@ -10,13 +17,8 @@ def main():
 def gameLoop(screen):
     white = (255, 255, 255)
     blackCell = pygame.image.load("../img/black.png")
-
-    #imageData = pygame.image.load("../map/3x3.png") # loading image
-    #datas = pygame.image.tostring(imageData, "RGBA")
-    #print(datas)
-    #print(len(datas) / 4)
     array = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
-    futureArray = [[]]
+    futureArray = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
     turn = 1
 
     while 1:
@@ -24,14 +26,17 @@ def gameLoop(screen):
             if event.type == pygame.QUIT: sys.exit()
         screen.fill(white)
         drawComponents(screen, blackCell, array, turn)
+        updateCells(array, futureArray)
+        turn += 1
         pygame.display.update() 
+        pygame.time.delay(700)
 
 def drawComponents(screen, blackCell, array, turn):
     blackCell = pygame.transform.scale(blackCell, (300, 300))
-    positionX = 0
-    positionY = 0
     font = pygame.font.Font('freesansbold.ttf', 16)
     text = font.render('Turn : ' + str(turn), True, (0, 255, 0)) 
+    positionX = 0
+    positionY = 0
 
     for subArray in array:
         for cell in subArray:
@@ -41,6 +46,58 @@ def drawComponents(screen, blackCell, array, turn):
         positionX = 0
         positionY += 300
     screen.blit(text, (10, 10))
+
+def updateCells(array, futureArray):
+    for line in range(3):
+        for column in range(3):
+            neighbour = getNeighbour(array, line, column)
+            if (array[line][column] == 1):
+                if (neighbour == 2):
+                    futureArray[line][column] = 1
+                else:
+                    futureArray[line][column] = 0
+            else:
+                if (neighbour == 3):
+                    futureArray[line][column] = 1
+                else:
+                    futureArray[line][column] = 0
+    for line in range(3):
+        for column in range(3):
+            array[line][column] = futureArray[line][column]
+
+def getNeighbour(array, line, column):
+    squareSize = 3
+    neighbour = 0
+    
+    #check uper line
+    if (line != 0):
+        if (array[line - 1][column] == 1):
+            neighbour += 1
+        #check left
+        if (column != 0 and array[line - 1][column - 1] == 1):
+            neighbour += 1
+        #check right
+        if (column != (squareSize - 1) and array[line - 1][column + 1] == 1):
+            neighbour += 1
+    #check left
+    if (column != 0):
+        if (array[line][column - 1] == 1):
+            neighbour += 1
+    #check right
+    if (column != (squareSize - 1)):
+        if (array[line][column + 1] == 1):
+            neighbour += 1
+    #check bottom line
+    if (line != (squareSize - 1)):
+        if (array[line + 1][column] == 1):
+            neighbour += 1
+        #check left
+        if (column != 0 and array[line + 1][column - 1] == 1):
+            neighbour += 1
+        #check right
+        if (column != (squareSize - 1) and array[line + 1][column + 1] == 1):
+            neighbour += 1
+    return neighbour
 
 if __name__ == "__main__":
     main()
