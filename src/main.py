e78a544e56d586149a7c7549c0568df23a72db48
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-08-28 12:49:34 +0200

    [ADD] - displaying the turn played

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 import sys, pygame
 
-array = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
-
 def main():
     pygame.init()
     size = width, height = (900, 900)
@@ -12,18 +10,28 @@ def main():
 def gameLoop(screen):
     white = (255, 255, 255)
     blackCell = pygame.image.load("../img/black.png")
-    
+
+    #imageData = pygame.image.load("../map/3x3.png") # loading image
+    #datas = pygame.image.tostring(imageData, "RGBA")
+    #print(datas)
+    #print(len(datas) / 4)
+    array = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+    futureArray = [[]]
+    turn = 1
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         screen.fill(white)
-        drawComponents(screen, blackCell)
-        pygame.display.flip()
+        drawComponents(screen, blackCell, array, turn)
+        pygame.display.update() 
 
-def drawComponents(screen, blackCell):
+def drawComponents(screen, blackCell, array, turn):
     blackCell = pygame.transform.scale(blackCell, (300, 300))
     positionX = 0
     positionY = 0
+    font = pygame.font.Font('freesansbold.ttf', 16)
+    text = font.render('Turn : ' + str(turn), True, (0, 255, 0)) 
 
     for subArray in array:
         for cell in subArray:
@@ -32,6 +40,7 @@ def drawComponents(screen, blackCell):
             positionX += 300
         positionX = 0
         positionY += 300
+    screen.blit(text, (10, 10))
 
 if __name__ == "__main__":
     main()
