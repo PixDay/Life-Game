b96eb60361c38d2c1525c38530f1620ce3a78b59
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-08-28 11:47:02 +0200

    [ADD] - BlackCell png object added to code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sys, pygame
 
+blackCell = pygame.image.load("../img/black.png")
+
 def main():
     pygame.init()
     size = width, height = 1024, 768
@@ -9,12 +11,16 @@ def main():
 
 def gameLoop(screen):
     white = 255, 255, 255
-
+    
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
         screen.fill(white)
+        #drawComponents(screen)
         pygame.display.flip()
+
+def drawComponents(screen):
+    print("drawComponents function")
 
 if __name__ == "__main__":
     main()
