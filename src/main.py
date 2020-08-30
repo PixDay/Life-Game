062565dd223e9043b3b156f7b74a61d1a241d5b2
Adrien Colombier <adrien.colombier@epitech.eu>
2020-08-30 15:20:02 +0200

    [SPLIT] - Split game function in game file

--- src/main.py
+++ src/main.py
@@ -21,29 +21,6 @@ def main():
 
     gameLoop(screen)
 
-def gameLoop(screen):
-    white = (255, 255, 255)
-    blackCell = pygame.image.load("img/black.png")
-    array = readMap()
-    futureArray = readMap()
-    turn = 1
-    aliveCell = 1
-
-    while aliveCell:
-        aliveCell = 0
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-        screen.fill(white)
-        drawComponents(screen, blackCell, array, turn)
-        updateCells(array, futureArray)
-        turn += 1
-        pygame.display.update() 
-        for subArray in array:
-            for cell in subArray:
-                if (cell == 1):
-                    aliveCell = 1
-        pygame.time.delay(20)        # 33 for 60 fps
-        
 def readMap():
     mapData = pygame.image.load(sys.argv[len(sys.argv) - 1]) # loading image
     datas = pygame.image.tostring(mapData, "RGBA")
@@ -81,59 +58,6 @@ def drawComponents(screen, blackCell, array, turn):
         positionX = 0
         positionY += (int)(windowSize / len(array))
     screen.blit(text, (10, 10))
-
-def updateCells(array, futureArray):
-    for line in range(len(array)):
-        for column in range(len(array)):
-            neighbour = getNeighbour(array, line, column)
-            if (array[line][column] == 1):
-                if (neighbour == 2 or neighbour == 3):
-                    futureArray[line][column] = 1
-                else:
-                    futureArray[line][column] = 0
-            else:
-                if (neighbour == 3):
-                    futureArray[line][column] = 1
-                else:
-                    futureArray[line][column] = 0
-    for line in range(len(array)):
-        for column in range(len(array)):
-            array[line][column] = futureArray[line][column]
-            futureArray[line][column] = 0
-
-def getNeighbour(array, line, column):
-    squareSize = len(array)
-    neighbour = 0
-    
-    #check uper line
-    if (line != 0):
-        if (array[line - 1][column] == 1):
-            neighbour += 1
-        #check left
-        if (column != 0 and array[line - 1][column - 1] == 1):
-            neighbour += 1
-        #check right
-        if (column != (squareSize - 1) and array[line - 1][column + 1] == 1):
-            neighbour += 1
-    #check left
-    if (column != 0):
-        if (array[line][column - 1] == 1):
-            neighbour += 1
-    #check right
-    if (column != (squareSize - 1)):
-        if (array[line][column + 1] == 1):
-            neighbour += 1
-    #check bottom line
-    if (line != (squareSize - 1)):
-        if (array[line + 1][column] == 1):
-            neighbour += 1
-        #check left
-        if (column != 0 and array[line + 1][column - 1] == 1):
-            neighbour += 1
-        #check right
-        if (column != (squareSize - 1) and array[line + 1][column + 1] == 1):
-            neighbour += 1
-    return neighbour
 
 if __name__ == "__main__":
     main()
