404b40bcf4b74c6872fcf22f958b9527761a0d3b
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-08-31 22:46:35 +0200

    [MODIFIED] - Remove the duplicated code moved to data.py file

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@
 ##
 
 import sys, pygame
-import math 
 import os
 
 from game import *
@@ -20,28 +19,6 @@ def main():
     screen = pygame.display.set_mode(size)
 
     gameLoop(screen)
-
-def readMap():
-    mapData = pygame.image.load(sys.argv[len(sys.argv) - 1]) # loading image
-    datas = pygame.image.tostring(mapData, "RGBA")
-    squareSize = (int)(math.sqrt((int)(len(datas) / 4)))
-    res = [[0 for i in range(squareSize)] for i in range(squareSize)]
-    x = 0
-    y = 0
-    cell = 0
-
-    for i in range(len(datas)):
-        if (i % 4 == 0):  
-            if (datas[i] == 0 and datas[i + 1] == 0 and datas[i + 2] == 0):
-                cell = 1
-            i += 4
-            res[y][x] = cell
-            x += 1
-            if (x == squareSize):
-                x = 0
-                y += 1
-            cell = 0
-    return res
 
 if __name__ == "__main__":
     main()
