c06c388c638dbbe37d8decccec9d90601b13e376
Adrien Colombier <33284435+PixDay@users.noreply.github.com>
2020-09-05 23:36:41 +0200

    [ADD] - menu principe is working files structure need to be done

--- src/menu.py
+++ src/menu.py
@@ -6,15 +6,28 @@
 ##
 
 import sys, pygame
-from main import *
-from graphic import *
-from data import *
+from game import *
+
+fps = 60
 
 def menuLoop(screen):
     clock = pygame.time.Clock()
+    white = (255, 255, 255)
     menu = 1
+    mousePosition = pygame.mouse.get_pos()
+    clicked = 0
 
     while menu:
-        clock.tick(60)
+        clock.tick(fps)
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+        screen.fill(white)
+        mousePosition = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN and clicked == 0:
+            clicked = 1
+        elif event.type != pygame.MOUSEBUTTONDOWN:
+            clicked = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                menu = 0
+        pygame.display.update()
